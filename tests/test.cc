168358fcd6d67f6b538e84aca8b962a8b6e946e0
sabarnimmagadda <sabarn2@illinois.edu>
2020-05-05 15:33:08 -0500

    Tested computations.

--- tests/test.cc
+++ tests/test.cc
@@ -4,12 +4,49 @@
 
 #include <catch2/catch.hpp>
 #include <cinder/Rand.h>
+#include <Linear Algebra/computations.h>
+#include <Eigen/Dense>
 
-#include <mylibrary/example.h>
 
 
 TEST_CASE("Random sanity test", "[random]") {
   const float random = cinder::randFloat();
   REQUIRE(0. <= random);
   REQUIRE(random <= 1.);
+}
+
+TEST_CASE("Computation tests") {
+    Eigen::Matrix3d test_mat;
+    test_mat << 1, 2, 3,
+            4, 5, 6,
+            7, 8, 9;
+    Eigen::Matrix3d test_mat2;
+    test_mat2 << 1, 2, 3,
+            4, 5, 6,
+            7, 8, 9;
+    std::stringstream ss;
+    std::stringstream os;
+
+    SECTION("RREF") {
+        REQUIRE(Computations::ComputeRREF(test_mat).size() == test_mat.size());
+    }
+    SECTION("Permutations") {
+        REQUIRE(Computations::ComputePermutationMatrix(test_mat).size() == test_mat.size());
+    }
+    SECTION("Multiplication") {
+        REQUIRE(Computations::ComputeDotProduct(test_mat, test_mat2, 3) == 285);
+    }
+    SECTION("Determinant") {
+        REQUIRE(Computations::ComputeDeterminant(test_mat) == 0);
+    }
+    SECTION("Matrix Content") {
+        ss << test_mat;
+        std::string str = "1 2 3\n"
+                          "4 5 6\n"
+                          "7 8 9";
+        REQUIRE(ss.str() == str);
+    }
+    SECTION("Multiplication Size") {
+        REQUIRE(Computations::ComputeMultiply(test_mat, test_mat2).size() == 9);
+    }
 }
